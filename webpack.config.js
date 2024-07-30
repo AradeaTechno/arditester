e1b36ed37e7e0ffd58b68163bfb8c51d651af75e
aradeatechno <aradeatechno@gmail.com>
2024-07-30 10:45:54 +0800

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    library: 'mydroptest',
+    libraryTarget: 'umd',
+    globalObject: 'this',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  externals: {
+    react: 'react',
+    'react-dom': 'react-dom',
+  },
+};
